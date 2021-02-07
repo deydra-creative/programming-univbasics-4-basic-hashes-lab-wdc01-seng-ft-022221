fa95af79a4d80b4c72c4b3452a2ab61f61d1a9b2
Deydra Cavazos <deydra@cavazospartners.com>
2021-02-07 01:54:45 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -3,14 +3,13 @@ def new_hash
 end
 
 def my_hash
-  my_hash = {"cat" => "Maru", "dog" => "Pluto"}
+  {"cat" => "Maru", "dog" => "Pluto"}
 end
 
 def pioneer
-  pioneer = {:name = "Grace Hopper"}
+  {:name = "Grace Hopper"}
 end
 
-
 def id_hash_generator(number)
-  id_hash_generator = .{id: 7}
+  {id: 7}
 end
